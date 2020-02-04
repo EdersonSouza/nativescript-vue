f9637c68fc9e3f55086d6da01ba2de29cac86269
Ederson Souza <ederson.souza.mg@gmail.com>
2020-02-04 16:18:15 -0400

    nativescript

---
+++ app/components/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar" title="Product"></ActionBar>
+        <GridLayout>
+            <ListView for="p in product" class="list-group">
+                <v-template>
+                    <StackLayout class="list-group-item">
+                        <Label :text="p.title" />
+                    </StackLayout>
+                </v-template>
+            </ListView>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    import * as http from "http";
+    export default {
+        data() {
+            return {
+                product: []
+            };
+        },
+        mounted() { 
+          http.getJSON("https://localhost:3000/products").then(result => {
+            this.product = result.results;
+            }, error => {
+              console.log(error);
+            });
+        }
+    };
+</script>
+
+<style scoped></style>
