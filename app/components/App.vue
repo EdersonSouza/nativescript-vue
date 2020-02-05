2b5938dfccdd8bcd247d29f604fdff9d55e8c2ae
Ederson Souza <ederson.souza.mg@gmail.com>
2020-02-05 10:05:08 -0400

    configurando ip

--- app/components/App.vue
+++ app/components/App.vue
@@ -23,7 +23,7 @@
             };
         },
         created() { 
-            axios({ method: "GET", "url": "http://192.168.1.3:3000/products" }).then(result => {
+            axios({ method: "GET", "url": "http://10.0.2.2:3000/products" }).then(result => {
                  this.products = result.data;
             }, error => {
                 console.error(error);
